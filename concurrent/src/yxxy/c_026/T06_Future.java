f13ab103f4481fad70f910f7ea3729dad6f9f7db
sunkun <87983133@qq.com>
2020-04-09 19:01:48 +0800

    注释补充

--- concurrent/src/yxxy/c_026/T06_Future.java
+++ concurrent/src/yxxy/c_026/T06_Future.java
@@ -10,9 +10,18 @@ import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 操作步骤：
+ * 一个类实现Callable接口，重新call方法；
+ * 在调用的时候，需要使用FutureTask这个类的有参构造，然后再使用thread的有参构造。
+ */
 public class T06_Future {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		
+		/***
+		 * new 了一个Callable对象 外面封装了一个FutureTask
+		 * Thread 启动的时候 不能往里面扔Callable 只能扔FutureTask
+		 * Callable 没有get方法接收返回值
+		 */
 		FutureTask<Integer> task = new FutureTask<>(()->{
 			TimeUnit.MILLISECONDS.sleep(500);
 			return 1000;
